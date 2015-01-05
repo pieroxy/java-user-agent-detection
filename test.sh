a2eb12e6046dd09ecfd2738eb70440b63850ff4d
pieroxy <pieroxy@pieroxy.net>
2015-01-05 19:44:06 +0100

    The release JAR does not contain the tests anymore.

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-java -cp release/user-agent-detector.jar net.pieroxy.ua.tooling.UserAgentTester test test-data/database.gz
+java -cp build/user-agent-detector.jar net.pieroxy.ua.tooling.UserAgentTester test test-data/database.gz
