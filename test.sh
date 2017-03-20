8270ddc4613d4750f36df1abdc773c2cf6a5463a
John Smith <mrdoctorwho@gmail.com>
2017-03-20 13:30:38 +0700

    Switch ant to maven

--- test.sh
+++ test.sh
@@ -1,1 +1,1 @@
-java -cp build/user-agent-detector.jar net.pieroxy.ua.tooling.UserAgentTester test test-data/database.gz
+java -cp target/user_agent_detector-2015-10-31-SNAPSHOT.jar net.pieroxy.ua.tooling.UserAgentTester test src/test/resources/database.gz
