5c0e33b59b96e094b2ce6f0bbd7eb18c9042ddc4
sam <sam1287@gmail.com>
2017-11-06 16:27:31 +0900

    Simple fix - Ensure not occurring to NullPointerException.ˆ

--- src/main/test/net/pieroxy/ua/tooling/UserAgentDetectorTest.java
+++ src/main/test/net/pieroxy/ua/tooling/UserAgentDetectorTest.java
@@ -7,8 +7,8 @@ import org.junit.Test;
  * @author sam
  */
 public class UserAgentDetectorTest {
-    @Test(expected = NumberFormatException.class)
-    public void testInCaseNullPointerException() {
+    @Test
+    public void testEnsureNullPointerExceptionBugFixed() {
         String userAgent = "Mozilla/5.0 (Linux; Android 6.0.1; Nexus 5 Build/MMB29Q; wv) AppleWebKit/537.36 (KHTML, like Gecko) Version/4.0 Chrome/48.0.2564.106 Mobile Safari/537.36 MobileApp/1.0 (Android; 4.0.3; com.ebay.kr.g9)";
         UserAgentDetector detector = new UserAgentDetector();
         detector.parseUserAgent(userAgent);
